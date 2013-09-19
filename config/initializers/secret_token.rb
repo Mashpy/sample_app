f4e6b659db11dd6cdbdc3af5eb2313cca80f1837
Mashpy <mashpy@mashpy-HP-G42-Notebook-PC.(none)>
2013-09-19 15:59:01 +0600

    Inial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '9934958a44cdfd1fe5a1ff72cb6483e51096b20bd28f80b2d4a9218174ca7a7954d524698a9eab455354a82b634c4ceefb547b81435096622126b362ffb54a5d'
